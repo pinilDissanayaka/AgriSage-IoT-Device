05f2247eeee022a97c696150fc72fbce37d79e75
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-08-30 14:11:37 +0530

    add npk sensor values

--- AgriSage-IoT/AgriSage-IoT.ino
+++ AgriSage-IoT/AgriSage-IoT.ino
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
 #include <DHT.h>
@@ -24,6 +26,16 @@
 #define Ph_PIN 40 // pin for ph sensor
 #define RELAY_PIN 12 //pin for water motor relay
 
+// Define RS485 pins for RE and DE to switch between transmit and receive mode
+#define RS485_RE 8
+#define RS485_DE 7
+
+const byte nitro[] = {0x01, 0x03, 0x00, 0x1e, 0x00, 0x01, 0xe4, 0x0c};
+const byte phos[] = {0x01, 0x03, 0x00, 0x1f, 0x00, 0x01, 0xb5, 0xcc};
+const byte pota[] = {0x01, 0x03, 0x00, 0x20, 0x00, 0x01, 0x85, 0xc0};
+
+SoftwareSerial modbus(2, 3); // RX, TX
+
 #define DHTTYPE DHT11
 DHT dht(DHT_PIN, DHTTYPE);
 
@@ -41,6 +53,16 @@ void setup(){
   Serial.begin(115200);
   dht.begin();
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  modbus.begin(9600);  // Start serial communication with the RS485 module
+
+  // Set RS485 pins as outputs
+  pinMode(RS485_RE, OUTPUT);
+  pinMode(RS485_DE, OUTPUT);
+
+  // Turn off RS485 receiver and transmitter initially
+  digitalWrite(RS485_RE, LOW);
+  digitalWrite(RS485_DE, LOW);
+
   Serial.print("Connecting to Wi-Fi");
   
   while (WiFi.status() != WL_CONNECTED){
@@ -83,7 +105,9 @@ void loop(){
 
   float humidity, temperature = getTemp();
   int soilMoisture = getSoilMoisture();
-  float calcium, nitrogen, potassium = getSoilNutrients();
+  byte nitrogen=readValue(nitro);
+  byte phosphorus=readValue(phos);
+  byte potassium=readValue(pota);
   int waterLevel= getWaterLevel();
   float phLavel=getPhLevel();
 
@@ -95,7 +119,7 @@ void loop(){
 
     Firebase.RTDB.setFloat(&fbdo, "1234/soilMoisture", soilMoisture);
 
-    Firebase.RTDB.setFloat(&fbdo, "1234/calcium", calcium);
+    Firebase.RTDB.setFloat(&fbdo, "1234/phosphorus", phosphorus);
     Firebase.RTDB.setFloat(&fbdo, "1234/nitrogen", nitrogen);
     Firebase.RTDB.setFloat(&fbdo, "1234/potassium", potassium);
 
@@ -119,6 +143,9 @@ float getTemp(){
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
+  Serial.println(humidity);
+  Serial.println(temperature);
+
 
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println(F("Failed to read from DHT sensor!"));
@@ -141,15 +168,29 @@ int getSoilMoisture(){
   }
 }
 
-float getSoilNutrients(){
-  delay(500);
-  float calcium, nitrogen, potassium;
+byte readValue(const byte* request) {
+  // Set RS485 module to transmit mode
+  digitalWrite(RS485_RE, HIGH);
+  digitalWrite(RS485_DE, HIGH);
 
-  calcium=random(0, 1000) / 1000;
-  nitrogen=random(0, 1200) / 1000;
-  potassium=random(0, 1100) / 1000;
+  // Send Modbus RTU request to the device
+  modbus.write(request, sizeof(request));
 
-  return calcium, nitrogen, potassium;
+  // Set RS485 module to receive mode
+  digitalWrite(RS485_RE, LOW);
+  digitalWrite(RS485_DE, LOW);
+
+  // Wait for the response to be received
+  delay(10);
+
+  // Read the response into the values array
+  byte responseLength = modbus.available();
+  for (byte i = 0; i < responseLength; i++) {
+    values[i] = modbus.read();
+  }
+
+  // Return the value from the response
+  return values[3] << 8 | values[4];
 }
 
 int getWaterLevel(){
